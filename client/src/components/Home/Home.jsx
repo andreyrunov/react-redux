ff8fe286915a4362774461f40f6ffcff464bc6c0
andrey <elefant86@bk.ru>
2022-09-08 18:55:43 +0300

    get data from api

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import './Home.css'
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+	// const list = [
+	// 	{
+	// 		id: '0XYvRd7oD',
+	// 		width: 1204,
+	// 		height: 1445,
+	// 		url: 'https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg',
+	// 	},
+	// 	{
+	// 		id: '0XYvRd7oD12',
+	// 		width: 1204,
+	// 		height: 1445,
+	// 		url: 'https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg',
+	// 	},
+	// 	{
+	// 		id: '0XYvRd7oD1212',
+	// 		width: 1204,
+	// 		height: 1445,
+	// 		url: 'https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg',
+	// 	},
+	// ]
+	const [list, setList] = useState([])
+
+	function handleClick() {
+		axios(`https://api.thecatapi.com/v1/images/search?limit=3&page1&order=Desc`)
+		.then(response => {
+			console.log(response)
+			setList(response.data)
+		})
+	}
+
+	return (
+		<div>
+			<button onClick={handleClick}	className='uk-button uk-button-secondary uk-button-large'>
+				Get cats
+			</button>
+			<ul className='uk-list'>
+				{list.map((cat) => (
+					<li key={cat.id}>
+						<div className='uk-card uk-card-default uk-card-body'>
+							<div className='foto'>
+								<img src={cat.url} alt={cat.id} />
+							</div>
+							<button class='uk-button uk-button-default'>💛</button>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
-export default Home;
+export default Home
