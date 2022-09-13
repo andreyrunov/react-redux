2a676e09b69aa90e604dbc74019b50ae6ccd76c4
andrey <elefant86@bk.ru>
2022-09-13 19:36:55 +0300

    add state & reducers

---
+++ client/src/components/redux/reducers/favourite.reducer.js
@@ -0,0 +1,8 @@
+export function favouriteReducer(state = [], action) {
+	const { type } = action
+	switch (type) {
+		default: {
+			return state
+		}
+	}
+}
---
+++ client/src/components/redux/reducers/list.reducer.js
@@ -0,0 +1,8 @@
+export function listReducer(state = [], action) {
+	const { type } = action
+	switch (type) {
+		default: {
+			return state
+		}
+	}
+}
